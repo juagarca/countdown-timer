9ac2dc6f81e9957784c622a5dd29d474fb94f582
Juan Garcia <juagarca@gmail.com>
2021-02-25 19:09:46 +0000

    progress bar functionality added to the main circle

--- app.js
+++ app.js
@@ -1,40 +1,46 @@
 let duration = 0;
-let interval = null;
+let interval;
 const play = document.querySelector('.play');
 
 const app = () => {
-  const outline = document.querySelector('.moving-outline circle');
-  const outlineLength = outline.getTotalLength();
+  const outline = setWhiteCircle();
   const timeDisplay = document.querySelector('.time-display');
-
-  outline.style.strokeDasharray = outlineLength;
-  outline.style.strokeDashoffset = outlineLength;
 
   play.addEventListener(("click"), () => {
     if (duration !== 0) {
-      runTimer(play, timeDisplay);
+      runTimer(play, timeDisplay, outline);
     }
   });
 }
 
-const runTimer = (play, timeDisplay) => {
+const runTimer = (play, timeDisplay, outlineLength, outline) => {
   if (play.src == 'file:///Users/juagarca/code/juagarca/countdown-timer/images/play.svg') {
     play.src = './images/pause.svg';
-    startTimer(timeDisplay);
+    startTimer(timeDisplay, outlineLength, outline);
   } else {
     play.src = './images/play.svg';
     stopTimer();
   }
 };
 
-const startTimer = (timeDisplay) => {
+const startTimer = (timeDisplay, outline) => {
+  let outlineLength = outline.getTotalLength();
+  const part = outlineLength / duration;
+  let progress = 0;
+
   interval = setInterval(function () {
       const minutes = Math.floor(duration / 60);
       const seconds = Math.floor(duration % 60);
       duration -= 1;
       setTimer(timeDisplay);
+      outlineLength -= part;
+
+      // outlineLength -= outlineLength / timer;
+      outline.style.strokeDashoffset = outlineLength;
+
       if (duration == 0) {
         stopTimer();
+        setWhiteCircle();
       }
   }, 1000);
 };
@@ -52,6 +58,14 @@ const setPlayImage = () => {
   play.src = "./images/play.svg";
 };
 
+const setWhiteCircle = () => {
+  const outline = document.querySelector('.moving-outline circle');
+  const outlineLength = outline.getTotalLength();
+  outline.style.strokeDasharray = outlineLength;
+  outline.style.strokeDashoffset = outlineLength;
+  return outline;
+}
+
 const setTimer = (timeDisplay) => {
   if (Math.floor(duration % 60) < 10) {
     timeDisplay.textContent = `${Math.floor(duration / 60)}:0${Math.floor(duration % 60)}`;
@@ -68,6 +82,7 @@ buttons.forEach((button) => {
   setDuration(event.currentTarget.getAttribute('data-time'));
   setPlayImage();
   stopTimer();
+  setWhiteCircle();
 
   const timeDisplay = document.querySelector('.time-display');
   setTimer(timeDisplay);
