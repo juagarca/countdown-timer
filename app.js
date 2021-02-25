bd66626284c8742de076cbe811f848f167adf10f
Juan Garcia <juagarca@gmail.com>
2021-02-25 18:28:04 +0000

    interval added to create the countdown

--- app.js
+++ app.js
@@ -1,39 +1,76 @@
+let duration = 0;
+const play = document.querySelector('.play');
+
 const app = () => {
   const outline = document.querySelector('.moving-outline circle');
   const outlineLength = outline.getTotalLength();
+  const timeDisplay = document.querySelector('.time-display');
 
   outline.style.strokeDasharray = outlineLength;
   outline.style.strokeDashoffset = outlineLength;
 
-  let duration = 100;
-
-  const play = document.querySelector('.play');
   play.addEventListener(("click"), () => {
-    // checkPlaying(song);
+    if (duration !== 0) {
+      runTimer(play, timeDisplay);
+    }
   });
 }
 
-const buttons = document.querySelectorAll('.time-select button');
-buttons.forEach((button) => {
- button.addEventListener(('click'), (event) => {
-  duration = event.currentTarget.getAttribute('data-time');
+const runTimer = (play, timeDisplay) => {
+  if (play.src == 'file:///Users/juagarca/code/juagarca/countdown-timer/images/play.svg') {
+    play.src = './images/pause.svg';
+    startTimer(timeDisplay);
+  } else {
+    play.src = './images/play.svg';
+  }
+};
 
-  const timeDisplay = document.querySelector('.time-display');
-  if (duration > 59) {
+const startTimer = (timeDisplay) => {
+  const interval = setInterval(function () {
+      const minutes = Math.floor(duration / 60);
+      const seconds = Math.floor(duration % 60);
+      duration -= 1;
+      console.log(duration);
+      setTimer(timeDisplay);
+      if (duration == 0) {
+        stopTimer(interval);
+      }
+  }, 1000);
+};
+
+const stopTimer = (interval) => {
+  clearInterval(interval);
+  setPlayImage();
+}
+
+const setDuration = (seconds) => {
+  duration = Number.parseInt(seconds, 10);;
+}
+
+const setPlayImage = () => {
+  play.src = "./images/play.svg";
+};
+
+const setTimer = (timeDisplay) => {
+  if (duration <= 59) {
+    timeDisplay.textContent = `${Math.floor(duration / 60)}:${Math.floor(duration % 60)}`;
+  } else if (Math.floor(duration % 60) === 0) {
     timeDisplay.textContent = `${Math.floor(duration / 60)}:${Math.floor(duration % 60)}0`;
   } else {
     timeDisplay.textContent = `${Math.floor(duration / 60)}:${Math.floor(duration % 60)}`;
   }
+};
+
+const buttons = document.querySelectorAll('.time-select button');
+buttons.forEach((button) => {
+ button.addEventListener(('click'), (event) => {
+  setDuration(event.currentTarget.getAttribute('data-time'));
+  setPlayImage();
+
+  const timeDisplay = document.querySelector('.time-display');
+  setTimer(timeDisplay);
  });
 });
 
-
-// const checkPlaying = (song) => {
-//   if(song.paused) {
-//     play.src = './images/pause.svg'
-//   } else {
-//     play.src = './images/play.svg'
-//   }
-// }
-
 app();
+
